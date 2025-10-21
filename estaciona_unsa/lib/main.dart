e12180486479b22a87c78b6cc1814e800936ae7e
Fernando Garambel <fgarambel@unsa.edu.pe>
2025-10-21 10:47:55 -0500

    fix: agregar async a main() para inicializar Firebase correctamente

--- estaciona_unsa/lib/main.dart
+++ estaciona_unsa/lib/main.dart
@@ -1,6 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';                        
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  // IMPORTANTE: Necesario para inicializar Firebase                      
+  WidgetsFlutterBinding.ensureInitialized();                              
+                                                                              
+  // Inicializar Firebase                                                 
+  await Firebase.initializeApp(                                           
+      options: DefaultFirebaseOptions.currentPlatform,                      
+  );
+  
   runApp(const MyApp());
 }
 
@@ -11,7 +21,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'EstacionaUNSA',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -30,7 +40,7 @@ class MyApp extends StatelessWidget {
         // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'EstacionaUNSA'),
     );
   }
 }
